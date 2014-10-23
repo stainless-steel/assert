e99585114a5a06c57bca814cc438e6f36688f96a
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-23 13:49:56 +0400

    Added assert_equal

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,20 @@
 #![feature(macro_rules)]
 
 #[macro_export]
+macro_rules! assert_equal(
+    ($left:expr , $right:expr) => ({
+        for (&x, &y) in $left.iter().zip($right.iter()) {
+            assert_eq!(x, y);
+        }
+    });
+)
+
+#[macro_export]
 macro_rules! assert_close(
     ($left:expr, $right:expr) => ({
         use std::num::abs;
         for (&x, &y) in $left.iter().zip($right.iter()) {
-            assert!(abs(x - y) < 1e-8);
+            assert!(abs(x - y) < 1e-8, "{} !~ {}", x, y);
         }
     });
 )
@@ -17,7 +26,7 @@ macro_rules! assert_abs_close(
     ($left:expr, $right:expr) => ({
         use std::num::abs;
         for (&x, &y) in $left.iter().zip($right.iter()) {
-            assert!(abs(abs(x) - abs(y)) < 1e-8);
+            assert!(abs(abs(x) - abs(y)) < 1e-8, "|{}| !~ |{}|", x, y);
         }
     });
 )
