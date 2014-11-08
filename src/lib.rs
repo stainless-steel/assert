4a9f04d4a3b68d0c8d6ba96f9d6ca5053cdf251b
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-08 17:24:13 +0100

    Combed the library; added some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,51 +2,104 @@
 
 #![feature(macro_rules)]
 
+/// Assert that the distance between the absolute values of the corresponding
+/// elements of two vectors is smaller than `delta!()`.
 #[macro_export]
-macro_rules! epsilon(
-    () => (::std::f64::EPSILON.sqrt());
+macro_rules! assert_abs_close(
+    ($x:expr, $y:expr) => ({
+        use std::num::abs;
+        let delta = delta!();
+        for (&x, &y) in $x.iter().zip($y.iter()) {
+            if (x as f64).is_finite() && (y as f64).is_finite() {
+                assert!(abs(abs(x) - abs(y)) < delta, "|{}| !~ |{}|", x, y);
+            } else {
+                assert_eq!(x, y);
+            }
+        }
+    });
 )
 
+/// Assert that the distance between the corresponding elements of two vectors
+/// is smaller than `delta!()`.
+#[macro_export]
+macro_rules! assert_close(
+    ($x:expr, $y:expr) => ({
+        use std::num::abs;
+        let delta = delta!();
+        for (&x, &y) in $x.iter().zip($y.iter()) {
+            if (x as f64).is_finite() && (y as f64).is_finite() {
+                assert!(abs(x - y) < delta, "{} !~ {}", x, y);
+            } else {
+                assert_eq!(x, y);
+            }
+        }
+    });
+)
+
+/// Assert that two vectors are equal.
 #[macro_export]
 macro_rules! assert_equal(
-    ($left:expr, $right:expr) => ({
-        for (&x, &y) in $left.iter().zip($right.iter()) {
+    ($x:expr, $y:expr) => ({
+        for (&x, &y) in $x.iter().zip($y.iter()) {
             assert_eq!(x, y);
         }
     });
 )
 
+/// Assert that the result is unsuccessful.
 #[macro_export]
-macro_rules! assert_close(
-    ($left:expr, $right:expr) => ({
-        use std::num::abs;
-        let epsilon = epsilon!();
-        for (&x, &y) in $left.iter().zip($right.iter()) {
-            if (x as f64).is_finite() && (y as f64).is_finite() {
-                assert!(abs(x - y) < epsilon, "{} !~ {}", x, y);
-            } else {
-                assert_eq!(x, y);
-            }
+macro_rules! assert_err(
+    ($result:expr) => {
+        match $result {
+            Ok(value) => assert!(false, "Ok({})", value),
+            Err(error) => error,
         }
-    });
+    };
 )
 
-#[macro_export]
-macro_rules! assert_abs_close(
-    ($left:expr, $right:expr) => ({
-        use std::num::abs;
-        let epsilon = epsilon!();
-        for (&x, &y) in $left.iter().zip($right.iter()) {
-            if (x as f64).is_finite() && (y as f64).is_finite() {
-                assert!(abs(abs(x) - abs(y)) < epsilon, "|{}| !~ |{}|", x, y);
-            } else {
-                assert_eq!(x, y);
-            }
-        }
-    });
-)
-
+/// Assert that the result is successful.
 #[macro_export]
 macro_rules! assert_ok(
-    ($result:expr) => (assert!($result.is_ok()));
+    ($result:expr) => {
+        match $result {
+            Ok(value) => value,
+            Err(error) => assert!(false, "Err({})", error),
+        }
+    };
 )
+
+/// Return the square root of the machine epsilon.
+#[macro_export]
+macro_rules! delta(
+    () => (::std::f64::EPSILON.sqrt());
+)
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn assert_equal() {
+        assert_equal!([1f64, 2.0, 3.0], [1f64, 2.0, 3.0]);
+    }
+
+    #[test]
+    fn assert_close() {
+        assert_close!([1f64, 2.0, 3.0], [1f64, 2.0 + 1e-10, 3.0 - 1e-10]);
+    }
+
+    #[test]
+    fn assert_abs_close() {
+        assert_abs_close!([1f64, 2.0, 3.0], [-1f64, 2.0 + 1e-10, -3.0 - 1e-10]);
+    }
+
+    #[test]
+    fn assert_err() {
+        fn work() -> Result<(), ()> { Err(()) }
+        assert_err!(work());
+    }
+
+    #[test]
+    fn assert_ok() {
+        fn work() -> Result<(), ()> { Ok(()) }
+        assert_ok!(work());
+    }
+}
