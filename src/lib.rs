e24ad60e73d17bd5a1e45aa42a176a7e92d772e7
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-01-08 08:07:04 +0100

    Updated to Rust’s master

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 //! Assertions for testing.
 
-#![feature(macro_rules)]
-
 /// Assert that the distance between the absolute values of the corresponding
 /// elements of two vectors is smaller than `delta!()`.
 #[macro_export]
