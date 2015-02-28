afdc55e109e90c02b87d3f74233f55e0f8f85661
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-02-28 07:47:33 +0100

    Added assert_within! and assert_abs_within!

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,41 @@
 //! Assertions for testing.
 
 /// Assert that the distance between the absolute values of the corresponding
-/// elements of two vectors is smaller than `delta!()`.
+/// elements of two vectors is smaller than the square root of the machine
+/// epsilon.
 #[macro_export]
+#[deprecated(reason = "use assert_abs_within!")]
 macro_rules! assert_abs_close(
-    ($x:expr, $y:expr) => ({
+    ($x:expr, $y:expr) => (
+        assert_abs_within!($x, $y, ::std::f64::EPSILON.sqrt())
+    );
+);
+
+/// Assert that the distance between the absolute values of the corresponding
+/// elements of two vectors is smaller than a given value.
+#[macro_export]
+macro_rules! assert_abs_within(
+    ($x:expr, $y:expr, $delta:expr) => ({
         use std::num::Float;
-        let delta = delta!();
+        let delta = $delta;
         for (&x, &y) in $x.iter().zip($y.iter()) {
             if x.is_finite() && y.is_finite() {
                 assert!((x.abs() - y.abs()).abs() < delta, "|{}| !~ |{}|", x, y);
             } else {
-                assert_eq!(x, y);
+                assert!(x == y, "|{}| !~ |{}|", x, y);
             }
         }
     });
 );
 
 /// Assert that the distance between the corresponding elements of two vectors
-/// is smaller than `delta!()`.
+/// is smaller than the square root of the machine epsilon.
 #[macro_export]
+#[deprecated(reason = "use assert_within!")]
 macro_rules! assert_close(
-    ($x:expr, $y:expr) => ({
-        use std::num::Float;
-        let delta = delta!();
-        for (&x, &y) in $x.iter().zip($y.iter()) {
-            if x.is_finite() && y.is_finite() {
-                assert!((x - y).abs() < delta, "{} !~ {}", x, y);
-            } else {
-                assert_eq!(x, y);
-            }
-        }
-    });
+    ($x:expr, $y:expr) => (
+        assert_within!($x, $y, ::std::f64::EPSILON.sqrt())
+    );
 );
 
 /// Assert that two vectors are equal.
@@ -49,7 +53,7 @@ macro_rules! assert_equal(
 macro_rules! assert_err(
     ($result:expr) => {
         match $result {
-            Ok(..) => assert!(false, "got Ok(..), expected Err(..)"),
+            Ok(..) => assert!(false, "got an OK, expected an error"),
             Err(..) => {},
         }
     };
@@ -61,15 +65,26 @@ macro_rules! assert_ok(
     ($result:expr) => {
         match $result {
             Ok(..) => {},
-            Err(..) => assert!(false, "got Err(..), expected Ok(..)"),
+            Err(..) => assert!(false, "got an error, expected an OK"),
         }
     };
 );
 
-/// Return the square root of the machine epsilon.
+/// Assert that the distance between the corresponding elements of two vectors
+/// is smaller than a given value.
 #[macro_export]
-macro_rules! delta(
-    () => (::std::f64::EPSILON.sqrt());
+macro_rules! assert_within(
+    ($x:expr, $y:expr, $delta:expr) => ({
+        use std::num::Float;
+        let delta = $delta;
+        for (&x, &y) in $x.iter().zip($y.iter()) {
+            if x.is_finite() && y.is_finite() {
+                assert!((x - y).abs() < delta, "{} !~ {}", x, y);
+            } else {
+                assert!(x == y, "{} !~ {}", x, y);
+            }
+        }
+    });
 );
 
 #[cfg(test)]
@@ -79,17 +94,22 @@ mod test {
 
     #[test]
     fn assert_abs_close() {
-        assert_abs_close!([1f64, 2.0, 3.0], [-1f64, 2.0 + 1e-10, -3.0 - 1e-10]);
+        assert_abs_close!([1.0, 2.0, 3.0], [-1.0, 2.0 + 1e-10, -3.0 - 1e-10]);
+    }
+
+    #[test]
+    fn assert_abs_within() {
+        assert_abs_within!([1.0, 2.0, 3.0], [-1.0, 2.0 + 1e-10, -3.0 - 1e-10], 2e-10);
     }
 
     #[test]
     fn assert_close() {
-        assert_close!([1f64, 2.0, 3.0], [1f64, 2.0 + 1e-10, 3.0 - 1e-10]);
+        assert_close!([1.0, 2.0, 3.0], [1.0, 2.0 + 1e-10, 3.0 - 1e-10]);
     }
 
     #[test]
     fn assert_equal() {
-        assert_equal!([1f64, 2.0, 3.0], [1f64, 2.0, 3.0]);
+        assert_equal!([1.0, 2.0, 3.0], [1.0, 2.0, 3.0]);
     }
 
     #[test]
@@ -102,5 +122,10 @@ mod test {
     fn assert_ok() {
         fn work() -> Result<Success, Failure> { Ok(Success) }
         assert_ok!(work());
+    }
+
+    #[test]
+    fn assert_within() {
+        assert_within!([1.0, 2.0, 3.0], [1.0, 2.0 + 1e-10, 3.0 - 1e-10], 2e-10);
     }
 }
