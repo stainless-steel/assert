40dcd230ab5d3d4e053feaee4e16556288e188fc
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-20 20:44:46 -0400

    Move traits to traits.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,7 @@
 //! Assertions for testing.
 
-use std::fmt::Debug;
-use std::ops::{Add, Div, Mul, Sub};
-
-/// A floating-point number.
-pub trait Float: Add<Output=Self> + Div<Output=Self> + Mul<Output=Self> + Sub<Output=Self> +
-                 Copy + Debug + PartialEq + PartialOrd
-{
-    fn abs(&self) -> Self;
-    fn is_finite(&self) -> bool;
-}
-
-/// A collection of floating-point numbers.
-pub trait Floats<'l> {
-    type Item: Float;
-    type Iterator: Iterator<Item=&'l Self::Item>;
-
-    fn iterate(self) -> Self::Iterator;
-}
-
-macro_rules! implement(
-    ($kind:ty) => (
-        impl Float for $kind {
-            #[inline(always)] fn abs(&self) -> Self { <$kind>::abs(*self) }
-            #[inline(always)] fn is_finite(&self) -> bool { <$kind>::is_finite(*self) }
-        }
-    );
-);
-
-impl<'l, I, F> Floats<'l> for I where I: IntoIterator<Item=&'l F>, F: Float {
-    type Item = F;
-    type Iterator = I::IntoIter;
-
-    #[inline(always)] fn iterate(self) -> Self::Iterator { self.into_iter() }
-}
-
-implement!(f32);
-implement!(f64);
+mod traits;
+pub use traits::{Float, Floats};
 
 /// Assert that the distance between the corresponding elements of two vectors
 /// is smaller than a given value.
