b80deffc9e74598c738c48b425335e6bc186f873
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-06 08:29:34 -0400

    Remove close_abs

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,9 @@
 mod traits;
 pub use traits::{Float, Floats};
 
-/// Assert that the distance between the corresponding elements of two vectors
-/// is smaller than a given value.
+/// Assert that the absolute difference between two quantities is small.
+///
+/// In case of vector-valued quantities, the assertion is elementwise.
 pub fn close<F, F1, F2>(x: F1, y: F2, delta: F) where F1: Floats<F>, F2: Floats<F>, F: Float {
     let (x, y) = (x.floats(), y.floats());
     assert_eq!(x.len(), y.len());
@@ -17,20 +18,6 @@ pub fn close<F, F1, F2>(x: F1, y: F2, delta: F) where F1: Floats<F>, F2: Floats<
     }
 }
 
-/// Assert that the distance between the absolute values of the corresponding
-/// elements of two vectors is smaller than a given value.
-pub fn close_abs<F, F1, F2>(x: F1, y: F2, delta: F) where F1: Floats<F>, F2: Floats<F>, F: Float {
-    let (x, y) = (x.floats(), y.floats());
-    assert_eq!(x.len(), y.len());
-    for (&x, &y) in x.iter().zip(y) {
-        if x.is_finite() && y.is_finite() {
-            assert!((x.abs() - y.abs()).abs() < delta, "|{:?}| !~ |{:?}|", x, y);
-        } else {
-            assert!(x == y, "|{:?}| !~ |{:?}|", x, y);
-        }
-    }
-}
-
 #[cfg(test)]
 mod test {
     #[test]
@@ -39,11 +26,6 @@ mod test {
         ::close(&[1.0], &[1.0 + 1e-10], 2e-10);
         ::close(vec![1.0], &[1.0 + 1e-10], 2e-10);
         ::close(&vec![1.0], &[1.0 + 1e-10], 2e-10);
-    }
-
-    #[test]
-    fn close_abs() {
-        ::close_abs(&[1.0], &[-1.0 + 1e-10], 2e-10);
     }
 
     #[should_panic]
