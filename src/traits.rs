f7772e24721bf0b04f198b36de1c76ee267336e5
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-07-20 16:06:36 +0200

    Implement Floats for arrays with up to 1 + 42 elements

--- src/traits.rs
+++ src/traits.rs
@@ -12,7 +12,7 @@ pub trait Floats<T: Float> {
 }
 
 macro_rules! implement(
-    ($kind:ty) => (
+    ($($kind:ty),*) => ($(
         impl Float for $kind {
             #[inline]
             fn abs(&self) -> Self {
@@ -31,11 +31,10 @@ macro_rules! implement(
                 unsafe { slice::from_raw_parts(self, 1) }
             }
         }
-    );
+    )*);
 );
 
-implement!(f32);
-implement!(f64);
+implement!(f32, f64);
 
 impl<T: Float> Floats<T> for Vec<T> {
     #[inline]
@@ -59,17 +58,15 @@ impl<'l, T: Float> Floats<T> for &'l [T] {
 }
 
 macro_rules! implement(
-    () => ();
-    ($number:expr, $($tail:tt)*) => (
-        impl<'l, T: Float> Floats<T> for &'l [T; $number] {
+    ($($count:expr),*) => ($(
+        impl<'l, T: Float> Floats<T> for &'l [T; $count] {
             #[inline]
             fn floats(&self) -> &[T] {
                 *self
             }
         }
-        implement!($($tail)*);
-    );
+    )*);
 );
 
-implement!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-           24, 25, 26, 27, 28, 29, 30, 31, 32,);
+implement!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+           23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42);
