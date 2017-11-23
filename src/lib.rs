b15eda711192b6f86604ea6fbc8bda4978cb5fc8
Simon Harris <simon@pearfalse.com>
2017-11-23 19:32:17 +0000

    assert_seq_eq to assert two sequences as equal

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,58 @@ where
     }
 }
 
+/// Assert that two sequences have the same length, and equal elements. This
+/// macro has similar iteration mechanics to [zip], except that sequences of
+/// different lengths will not equate to each other.
+#[macro_export]
+macro_rules! assert_seq_eq (
+    ($left:expr, $right:expr) => (
+        _assert_seq_eq!($left, $right,
+            super::assert_seq_eq_deref_default,
+            super::assert_seq_eq_deref_default);
+    );
+
+    ($left:expr, $right:expr, $left_deref:expr, $right_deref:expr) => (
+        _assert_seq_eq!($left, $right, $left_deref, $right_deref);
+    );
+);
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! _assert_seq_eq (
+    ($left:expr, $right:expr, $left_deref:expr, $right_deref:expr) => (
+        let mut iter_left = $left.into_iter();
+        let mut iter_right = $right.into_iter();
+        let mut index = 0usize;
+
+        loop {
+            let (item_left, item_right) = (iter_left.next(), iter_right.next());
+
+            // Ensure that both iterators are still yielding items, or that
+            // they have both stopped
+            assert_eq!(item_left.is_none(), item_right.is_none(),
+                "Sequences differ at index {}; {} sequence stops early",
+                index, if item_left.is_none() { "left" } else { "right" });
+
+            if item_left.is_none() {
+                break;
+            }
+
+            let item_left = $left_deref(item_left.as_ref().unwrap());
+            let item_right = $right_deref(item_right.as_ref().unwrap());
+            assert_eq!(item_left, item_right,
+                "Sequences differ at index {}. Failed on: `{:?} == {:?}`",
+                index, item_left, item_right);
+            index += 1;
+        }
+    );
+);
+
+#[inline(always)]
+#[doc(hidden)]
+pub fn assert_seq_eq_deref_default<T>(val: T) -> T { val }
+
+
 #[cfg(test)]
 mod test {
     #[test]
@@ -42,5 +94,43 @@ mod test {
     #[test]
     fn close_zero() {
         ::close(vec![1.0], vec![1.0], 0.0);
+    }
+
+    #[test]
+    fn sequence_equal() {
+        let left = &[1, 2, 3];
+        let right = &[11, 12, 13];
+        assert_seq_eq!(left.iter().cloned(), right.iter().map(|&x| x - 10));
+    }
+
+    #[test]
+    #[should_panic]
+    fn sequence_equal_not_equal() {
+        let left = &[1, 2, 3];
+        let right = &[4, 5, 6];
+        assert_seq_eq!(left.iter(), right.iter());
+    }
+
+    #[test]
+    #[should_panic]
+    fn sequence_equal_left_short() {
+        let shorter = &[1, 2, 3];
+        let longer = &[1, 2, 3, 4];
+        assert_seq_eq!(shorter.iter(), longer.iter());
+    }
+
+    #[test]
+    #[should_panic]
+    fn sequence_equal_right_short() {
+        let shorter = &[1, 2, 3];
+        let longer = &[1, 2, 3, 4];
+        assert_seq_eq!(longer.iter(), shorter.iter());
+    }
+
+    #[test]
+    fn sequence_equal_custom_deref() {
+        let left = &[1, 2, 3]; // iterates as references
+        let right = &[11, 12, 13]; // iterates as values
+        assert_seq_eq!(left.iter(), right.iter().map(|&x| x - 10), |&x| x, |x| x);
     }
 }
